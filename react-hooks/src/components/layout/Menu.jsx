a5c3924d379aa4cd9d163a984f0e9faa2ad046ae
Alex Freitas SOARES <alex-freitas.soares.ext@bureauveritas.com>
2022-05-23 23:21:19 -0300

    refact

--- react-hooks/src/components/layout/Menu.jsx
+++ react-hooks/src/components/layout/Menu.jsx
@@ -3,48 +3,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Menu = props => {
-    function on() {
-        document.getElementById("overlay").style.display = "block";
-      }
-      
-      function off() {
-        document.getElementById("overlay").style.display = "none";
-      }
-
     return(
-    <aside className="Menu">
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Início</Link>
-                </li>
-                <li>
-                    <Link to="/useState">useState()</Link>
-                </li>
-                <li>
-                    <Link to="/useEffect">useEffect()</Link>
-                </li>
-                <li>
-                    <Link to="/useRef">useRef()</Link>
-                </li>
-                <li>
-                    <Link to="/useMemo">useMemo()</Link>
-                </li>
-                <li>
-                    <Link to="/useCallback">useCallback()</Link>
-                </li>
-                <li>
-                    <Link to="/useContext">useContext()</Link>
-                </li>
-                <li>
-                    <Link to="/useReducer">useReducer()</Link>
-                </li>
-                <li>
-                    <Link to="/useCustom">useMyHook()</Link>
-                </li>
-            </ul>
-        </nav>
-    </aside>
+        <aside className="Menu">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Início</Link>
+                    </li>
+                    <li>
+                        <Link to="/useState">useState()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useEffect">useEffect()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useRef">useRef()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useMemo">useMemo()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useCallback">useCallback()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useContext">useContext()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useReducer">useReducer()</Link>
+                    </li>
+                    <li>
+                        <Link to="/useCustom">useMyHook()</Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
 )
 }
 export default Menu
