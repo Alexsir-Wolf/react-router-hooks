a2a8379e41e26ed44e7c73be8d78b8931dd51821
Alex Freitas SOARES <alex-freitas.soares.ext@bureauveritas.com>
2022-05-27 14:26:39 -0300

    useEfect

--- react-hooks/src/views/examples/UseEffect.jsx
+++ react-hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(0);
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div>
+                <span className="text">Fatorial:</span>
+                <span className="text red">100</span>
+            </div>
+            <div className='center'>
+                <input 
+                    type="number" 
+                    className="input" 
+                    value={number}
+                    onChange={e => setNumber(e.target.value)}
+                    />
+            </div>
+
         </div>
     )
 }
